e63557360f597110debbc5cf365c0c231efc3b82
krish <krishna58lagunas@gmail.com>
2023-12-13 16:53:24 -0300

    1.1

--- app/src/main/java/com/example/notesdevie1/CarpetasActivity.java
+++ app/src/main/java/com/example/notesdevie1/CarpetasActivity.java
@@ -1,14 +1,29 @@
 package com.example.notesdevie1;
 
+import static android.os.Environment.getExternalStorageDirectory;
+
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
 public class CarpetasActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_carpetas);
+
     }
 }
